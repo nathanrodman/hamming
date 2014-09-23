4d5bc7b58837c6baabf4566aa30572bfc11f553e
Nathan Rodman <Nathan Rodman>
2014-09-23 12:54:56 -0700

    Changed the block so that there would be less code and much more simplified

--- hamming.rb
+++ hamming.rb
@@ -2,8 +2,7 @@ class Hamming
 	
 	def self.compute (string_a, string_b)
 		distance = 0
-		min_length = [string_a.length, string_b.length].min
-		min_length.times do |i|
+		[string_a.length, string_b.length].min.times do |i|
 			distance += 1 if string_a[i] != string_b[i]
 		end
 		distance
